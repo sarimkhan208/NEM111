1ac5ad1b78e39cfbcc27d11cc1c07539ad863023
sarimkhan208 <sarimkhan7275@gmail.com>
2023-04-08 22:32:10 +0530

    read and write the server has been done

--- sprint1/day-4/index.js
+++ sprint1/day-4/index.js
@@ -1,10 +1,29 @@
 const http = require("http")
+const fs = require("fs")
+
+
 
 const server = http.createServer((req,res)=>{
     if(req.url=="/"){
         res.end("this is home page")
     }else if(req.url=="/about"){
         res.end("this is about page")
+    }
+    else if(req.url == "/data"){
+        try{
+            const data = fs.readFileSync("./data.json","utf-8")
+            res.end(data)
+        }catch(err){
+            res.end(err)
+        }
+    }
+    else if(req.url=="/writedata"){
+        try{
+            const data = fs.writeFileSync("./text.txt","This is me Learning the logic of server")
+            res.end("Data has been written")
+        }catch(err){
+            res.end(err)
+        }
     }
 })
 
