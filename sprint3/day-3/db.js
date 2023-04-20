77d68f5a869e35c06a617db15f148150d24cbea0
sarimkhan208 <sarimkhan7275@gmail.com>
2023-04-20 22:20:52 +0530

    MVC

---
+++ sprint3/day-3/db.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const mongoose = require("mongoose")
+
+const connection = mongoose.connect(process.env.mongoURL)
+
+const userSchema = mongoose.Schema({
+    name:{type:String,required:true},
+    age:{type:Number,required:true},
+    isMarried:{type:Boolean,required:true},
+    city:{type:String,required:true}
+},{versionKey:false})
+
+
+const UserModel = mongoose.model("user",userSchema)
+
+
+module.exports = {connection,UserModel}
+
+
